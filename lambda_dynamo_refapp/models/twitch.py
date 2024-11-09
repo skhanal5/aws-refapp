76993e4fa6a1b9191a4c1c5f0a427c656e99cb22
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-11-09 14:50:32 -0500

    Add model classes used in API calls from Twitch

---
+++ lambda_dynamo_refapp/models/twitch.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+
+class UserRequest(BaseModel):
+    user: str
+
+class ClipsInfo(BaseModel):
+    id: str
+    url: str
+    embed_url: str
+    broadcaster_id: str
+    broadcaster_name: str
+    creator_id: str
+    creator_name: str
+    video_id: str
+    game_id: str
+    language: str
+    title: str
+    view_count: int
+    created_at: str
+    thumbnail_url: str
+    duration: float
+    vod_offset: int
+    is_featured: bool
+
+class Pagination(BaseModel):
+    cursor: str
+class ClipsResponse(BaseModel):
+    data: list[ClipsInfo]
+    pagination: Pagination
