09cfc138f20d3a1db0328eeba409a148a8132524
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-11-09 14:52:44 -0500

    Add lambda handler implementation

---
+++ lambda_dynamo_refapp/lambdas/handler.py
@@ -0,0 +1,44 @@
+import os
+from typing import List, Annotated
+
+import boto3
+from aws_lambda_powertools.event_handler import ALBResolver, Response
+from aws_lambda_powertools.event_handler.openapi.params import Body
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+from lambda_dynamo_refapp.models.twitch import User, ClipsResponse
+from lambda_dynamo_refapp.services.clips_service import ClipsService
+from lambda_dynamo_refapp.services.twitch_service import TwitchService
+
+# logger = Logger()
+app = ALBResolver(enable_validation=True)
+
+# Load Env
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+# Setup Dynamo
+dynamodb = boto3.resource("dynamodb")
+users_table = dynamodb.Table("clips")
+clips_table = dynamodb.Table("clips")
+
+# Load Services
+twitch_service = TwitchService(client_id, client_secret)
+clips_service = ClipsService(users_table)
+
+@app.post("/user")
+def post_user(user: str) -> Response[dict]:
+    clips = twitch_service.get_clips_from_broadcaster(user)
+    clips_service.put_clips(user, clips)
+    return Response(
+        status_code=204,
+        body={"message": "added user"}
+    )
+
+@app.get("/users")
+def trigger_job():
+    return clips_service.scan()
+
+# Register handler function
+def lambda_handler(event: dict, context: LambdaContext):
+    return app.resolve(event, context)
